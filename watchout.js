22cf53c7686f2c6fece80683958785b0501aaab9
Catherine Bui <contactcatherinebui@gmail.com>
2014-11-07 14:24:43 -0800

    blah

--- watchout.js
+++ watchout.js
@@ -1,2 +1,30 @@
-// start slingin' some d3 here.
+//create svg node append to body
+var svg = d3.select("body").append("svg")
+    .attr("width", 1200)
+    .attr("height", 600);
 
+//creates an array of n enemies with id and random position
+var createEnemyData = function(n) {
+  return _.range(n).map(function(item) {
+    return {id: item, x: Math.random() * 1200, y: Math.random() * 650}
+  });
+};
+
+
+var appendEnemies = function(enemyData) {
+  return svg.selectAll('circle').data(enemyData).enter()
+    .append('circle')
+    .style('fill', function(d, i) {
+      return i%2 === 0 ? '#D3DACC' : '#E7C597'
+    })
+    .attr('r','2%')
+    .attr('cx', function(d) {
+      return d.x;
+    })
+    .attr('cy', function(d) {
+      return d.y;
+    });
+};
+
+var enemies = createEnemyData(20);
+appendEnemies(enemies);
